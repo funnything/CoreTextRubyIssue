193a1d69de8627e7ee6e89396f2743c77ba120e1
funnything <coding.while.sleep@gmail.com>
2019-11-17 14:30:16 +0900

    Init / gibo dump Windows macOS Linux Xcode Emacs Vim Swift Objective-C > .gitignore

---
+++ CoreTextRubyIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreTextRubyIssue
+//
+//  Created by Yosaku Toyama on 2019/11/17.
+//  Copyright © 2019 Yosaku Toyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
